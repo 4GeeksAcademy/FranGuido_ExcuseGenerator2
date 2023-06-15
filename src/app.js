a826fedf674d83d35f083264d002ee6ab9808cf0
Fran Guido <114300538+francinieguido@users.noreply.github.com>
2023-06-15 01:50:41 +0000

    Updated comments to clarify document

--- src/app.js
+++ src/app.js
@@ -5,12 +5,15 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+//load new excuse when page reloads//
 window.onload = function() {
   //write your code here
+  //selecting html file for showing the excuse//
   document.querySelector("#theexcuse").innerHTML = generateExcuse();
 };
 
-let generateExcuse = () => {
+  //Excuse components//
+  let generateExcuse = () => {
   let who = [
     "The police",
     "My neighbor",
@@ -26,12 +29,13 @@ let generateExcuse = () => {
     "during my lunch."
   ];
 
-  //index selector//
+  //index for selecting excuse components//
   let whoIndex = Math.floor(Math.random() * who.length);
   let actionIndex = Math.floor(Math.random() * action.length);
   let whatIndex = Math.floor(Math.random() * what.length);
   let whenIndex = Math.floor(Math.random() * when.length);
 
+  //concatenating and returning the excuse//
   return (
     who[whoIndex] +
     " " +
